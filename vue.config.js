2e12079b0a3a3113bd2f11126ee9682d5f5934c8
sudorogamen <sudorogamen@bk.ru>
2024-07-24 14:24:44 +0300

    добвлен gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/Vue3-products-/'
-      : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/Vue-Firebase/" : "/",
+})
