429ca2c4cfd19c30df12459e6a8b120b7368a9db
sudorogamen <sudorogamen@bk.ru>
2024-07-24 14:49:31 +0300

    добавлен gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue3-products/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue3-products-/" : "/",
 })
