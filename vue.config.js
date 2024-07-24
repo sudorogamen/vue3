b03ea89d5fe9bb93a5170c681748f2e74075bdca
sudorogamen <sudorogamen@bk.ru>
2024-07-24 15:28:54 +0300

    измененны данные

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:`/vue3/`,
+  publicPath: process.env.NODE_ENV === "production" ? "/vue3/" : "/",
 })
